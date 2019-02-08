36eddc8ddb1fb998c0c8fe99669574a1f0e131af
Robert <rkhousadian@protonmail.com>
2019-02-07 23:33:04 -0800

    accept apple quotes because they're special

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func init() {
 }
 
 func main() {
-	var Token string = config.Token
+	var Token string = config.Token //Add bot token here
 	dg, err := discordgo.New("Bot " + Token)
 	if err != nil {
 		fmt.Println("Error occurred creating discord session - ", err)
@@ -102,7 +102,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 					s.ChannelMessageSend(m.ChannelID, "Okay, " + m.Author.Mention() + "!\n" +
 						"I have successfully added your spawn to the spawn channel.")
 					fmt.Println(strings.Title(strings.ToLower(getArgs[1])) + " spawn reported by: " + m.Author.Username)
-				}else{
+				}else if len(getArgs) == 3{
 					embed := &discordgo.MessageEmbed{
 						Author:      &discordgo.MessageEmbedAuthor{},
 						Color:       0x00ff00,
@@ -122,6 +122,9 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 					s.ChannelMessageSend(m.ChannelID, "Okay, " + m.Author.Mention() + "!\n" +
 						"I have successfully added your spawn to the spawn channel.")
 					fmt.Println(strings.Title(strings.ToLower(getArgs[1])) + " spawn reported by: " + m.Author.Username)
+				}else{
+					s.ChannelMessageSend(m.ChannelID, "You ran the command incorrectly.\n" +
+						"Please see the how-to: https://goo.gl/ckdYbE")
 				}
 			}else{
 				s.ChannelMessageSend(m.ChannelID, m.Author.Mention() + ", `" + strings.Title(getArgs[1]) + "` is not a Pokemon!")
@@ -135,37 +138,56 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	if getArgs[0] == config.Prefix + "quest"{
+		var getQuestArgs []string
+		var stopName string
+		var questType string
+		var questReward string
+		var stopInformation map[string]string
+		var check bool
 		if strings.Contains(m.Content, "\"") == true{
-			getQuestArgs := strings.Split(m.Content, "\"")
-			stopName := getQuestArgs[1]
-			questType := getQuestArgs[3]
-			questReward := getQuestArgs[5]
-			stopInformation := functions.GrabStopInformation(stopName)
-			if len(stopInformation) >= 3 {
-				embed := &discordgo.MessageEmbed{
-					Author: &discordgo.MessageEmbedAuthor{},
-					Color:  0x00ff00,
-					Description: "**Reset: **12:00am\n" +
-						"**Quest: **" + strings.Title(questType) + "\n" +
-						"**Reward: ** " + strings.Title(questReward) + "\n" +
-						"**Reported by: **" + m.Author.Mention(),
-					Thumbnail: &discordgo.MessageEmbedThumbnail{
-						URL: stopInformation["img"],
-					},
-					Footer: &discordgo.MessageEmbedFooter{
-						Text:    "Created by github.com/rkhous",
-						IconURL: "https://d1q6f0aelx0por.cloudfront.net/product-logos/81630ec2-d253-4eb2-b36c-eb54072cb8d6-golang.png"},
-					Title: "**" + stopInformation["name"] + " - Click for directions!**",
-					URL:   "http://maps.google.com/maps?daddr=" + stopInformation["lat,lon"] + "&amp;ll=",
-				}
-				reportedQuest, _ := s.ChannelMessageSendEmbed(config.QuestChannel, embed)
-				config.LiveQuests = append(config.LiveQuests, reportedQuest.ID)
-				fmt.Println("User " + m.Author.Username + " has reported a new quest.")
-				s.ChannelMessageSend(m.ChannelID, "Okay, "+ m.Author.Mention() + "! Your quest was reported successfully.")
-			}else{
-				fmt.Println("len is not greater")
-				s.ChannelMessageSend(m.ChannelID, "Pokestop not found!")
+			getQuestArgs = strings.Split(m.Content, "\"")
+			stopName = getQuestArgs[1]
+			questType = getQuestArgs[3]
+			questReward = getQuestArgs[5]
+			stopInformation = functions.GrabStopInformation(stopName)
+			check = true
+		}else if strings.Contains(m.Content, "“") == true{
+			getQuestArgs = strings.Split(m.Content, "“")
+			stopName = getQuestArgs[1]
+			questType = getQuestArgs[3]
+			questReward = getQuestArgs[5]
+			stopInformation = functions.GrabStopInformation(stopName)
+			check = true
+		}else{
+			check = false
+		}
+		if len(stopInformation) >= 3 && check == true {
+			embed := &discordgo.MessageEmbed{
+				Author: &discordgo.MessageEmbedAuthor{},
+				Color:  0x00ff00,
+				Description: "**Reset: **12:00am\n" +
+					"**Quest: **" + strings.Title(questType) + "\n" +
+					"**Reward: ** " + strings.Title(questReward) + "\n" +
+					"**Reported by: **" + m.Author.Mention(),
+				Thumbnail: &discordgo.MessageEmbedThumbnail{
+					URL: stopInformation["img"],
+				},
+				Footer: &discordgo.MessageEmbedFooter{
+					Text:    "Created by github.com/rkhous",
+					IconURL: "https://d1q6f0aelx0por.cloudfront.net/product-logos/81630ec2-d253-4eb2-b36c-eb54072cb8d6-golang.png"},
+				Title: "**" + stopInformation["name"] + " - Click for directions!**",
+				URL:   "http://maps.google.com/maps?daddr=" + stopInformation["lat,lon"] + "&amp;ll=",
 			}
+			reportedQuest, _ := s.ChannelMessageSendEmbed(config.QuestChannel, embed)
+			config.LiveQuests = append(config.LiveQuests, reportedQuest.ID)
+			fmt.Println("User " + m.Author.Username + " has reported a new quest.")
+			s.ChannelMessageSend(m.ChannelID, "Okay, "+ m.Author.Mention() + "! Your quest was reported successfully.")
+		}else if check != true{
+			s.ChannelMessageSend(m.ChannelID, "You have ran the command incorrectly.\n" +
+				"Please see the how-to guide: https://goo.gl/ckdYbE")
+		}else{
+			fmt.Println("len is not greater")
+			s.ChannelMessageSend(m.ChannelID, "Pokestop not found!")
 		}
 	}
 
