77cc372fbac0d62a4f5c57567aa1c72eb57716d1
Robert <rkhousadian@gmail.com>
2019-08-26 21:09:21 -0700

    add TR channel configuration

--- config/config.go
+++ config/config.go
@@ -5,4 +5,5 @@ var Prefix string = "." //limited to one char, the bots prefix, default = .
 var Token string = "FaKeT0k3n" //the bots token, provided by discord
 var SpawnsChannel string = "123456789" //ID for the spawns channel
 var QuestChannel string = "123456789" //ID for the quests channel
+var TeamRocketChannel string = "" //ID for team rocket pokestops channel
 var LiveQuests []string = []string{""} //DO NOT EDIT THIS! Leave blank!
